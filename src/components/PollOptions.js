8258ca089f8927ed5e2cb7039834a7d7f2cc4127
GeorgeDubuque <34558994+GeorgeDubuque@users.noreply.github.com>
2024-03-13 06:52:38 -0700

    added buttons for navigating between pages

--- src/components/PollOptions.js
+++ src/components/PollOptions.js
@@ -1,184 +1,212 @@
-import { Box, Button, Grid, Paragraph, Text, TextInput } from 'grommet';
-import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid'; // Import UUID generator
-import copy from 'copy-to-clipboard';
-import axios from 'axios';
-import { API, graphqlOperation } from "aws-amplify";
-import { listPolls } from "../graphql/queries";
+import { Box, Button, Grid, Paragraph, Text, TextInput } from "grommet"
+import React, { useState } from "react"
+import { v4 as uuidv4 } from "uuid" // Import UUID generator
+import copy from "copy-to-clipboard"
+import axios from "axios"
+import { API, graphqlOperation } from "aws-amplify"
+import { listPolls } from "../graphql/queries"
 import {
-    createPoll as createPollMutation,
-    createOption as createOptionMutation,
-} from "../graphql/mutations";
+	createPoll as createPollMutation,
+	createOption as createOptionMutation,
+} from "../graphql/mutations"
 
-import $ from 'jquery';
-import { generatePollText, getOrSetUserId } from '../utility/utilities';
-import { Add, AddCircle, Close, FormAdd, FormClose, Trash } from 'grommet-icons';
-import { Icon } from '@aws-amplify/ui-react';
-
+import $ from "jquery"
+import { generatePollText, getOrSetUserId } from "../utility/utilities"
+import { Add, AddCircle, Close, FormAdd, FormClose, Trash } from "grommet-icons"
+import { Icon } from "@aws-amplify/ui-react"
 
 const PollOptions = () => {
-    const [options, setOptions] = useState([]);
-    const [isPollGenerated, setIsPollGenerated] = useState(false);
-    const [generatedPoll, setGeneratedPoll] = useState("");
-    const [description, setDescription] = useState("");
+	const [options, setOptions] = useState([])
+	const [isPollGenerated, setIsPollGenerated] = useState(false)
+	const [generatedPoll, setGeneratedPoll] = useState("")
+	const [description, setDescription] = useState("")
 
-    const addOption = async () => {
-        const newOption = { id: uuidv4(), text: '' }; // Generate unique ID
-        setOptions([...options, newOption]);
-    };
+	const addOption = async () => {
+		const newOption = { id: uuidv4(), text: "" } // Generate unique ID
+		setOptions([...options, newOption])
+	}
 
-    const removeOption = (id) => {
-        const updatedOptions = options.filter(option => option.id !== id);
-        setOptions(updatedOptions);
-    };
+	const removeOption = (id) => {
+		const updatedOptions = options.filter((option) => option.id !== id)
+		setOptions(updatedOptions)
+	}
 
-    const handleOptionChange = (id, newText) => {
-        const updatedOptions = options.map(option =>
-            option.id === id ? { ...option, text: newText } : option
-        );
-        setOptions(updatedOptions);
-    };
+	const handleOptionChange = (id, newText) => {
+		const updatedOptions = options.map((option) =>
+			option.id === id ? { ...option, text: newText } : option
+		)
+		setOptions(updatedOptions)
+	}
 
-    const handleDescriptionChange = (newDescription) => {
-        setDescription(newDescription);
-    };
+	const handleDescriptionChange = (newDescription) => {
+		setDescription(newDescription)
+	}
 
-    const createPoll = async () => {
+	const createPoll = async () => {
+		// generate user id if not in cookies otherwise get from cookies
+		let userId = getOrSetUserId()
 
-        // generate user id if not in cookies otherwise get from cookies
-        let userId = getOrSetUserId();
+		const createPollParams = {
+			input: { description: description, creatorId: userId },
+		}
 
-        const createPollParams = {
-            input: { description: description, creatorId: userId }
-        }
+		const createPollResult = await API.graphql(
+			graphqlOperation(createPollMutation, createPollParams)
+		)
 
-        const createPollResult = await API.graphql(
-            graphqlOperation(createPollMutation, createPollParams)
-        );
+		const poll = createPollResult.data.createPoll
 
-        const poll = createPollResult.data.createPoll;
+		console.log(`poop`)
 
-        console.log(`poop`);
+		// Make a GET request to your serverless function
+		const apiUrl =
+			"https://xfm6ahnlme.execute-api.us-west-2.amazonaws.com/default/easyPollBulkRequestTinyUrl-staging"
 
+		const baseUrl = window.location.origin
 
-        // Make a GET request to your serverless function
-        const apiUrl = 'https://xfm6ahnlme.execute-api.us-west-2.amazonaws.com/default/easyPollBulkRequestTinyUrl-staging';
+		// Data to send in the request body (assuming it's in JSON format)
+		const requestData = {
+			baseUrl,
+			options,
+			poll,
+		}
+		console.log("requestData:", requestData)
 
-        const baseUrl = window.location.origin;
+		const response = await fetch(apiUrl, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json", // Specify the content type as JSON
+			},
+			body: JSON.stringify(requestData), // Convert data to JSON string
+		})
 
-        // Data to send in the request body (assuming it's in JSON format)
-        const requestData = {
-            baseUrl,
-            options,
-            poll
-        };
-        console.log('requestData:', requestData);
+		if (!response.ok) {
+			throw new Error(
+				`Error calling the tiny url lambda functions ${response.status}`
+			)
+		}
 
-        const response = await fetch(apiUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json', // Specify the content type as JSON
-            },
-            body: JSON.stringify(requestData), // Convert data to JSON string
-        });
+		const responseJson = await response.json()
+		console.log("responseJson: ", responseJson)
 
-        if (!response.ok) {
-            throw new Error(`Error calling the tiny url lambda functions ${response.status}`);
-        }
+		let optionsToCreate = responseJson.body.optionsToCreate
 
-        const responseJson = await response.json();
-        console.log("responseJson: ", responseJson);
+		let optionsList = []
 
-        let optionsToCreate = responseJson.body.optionsToCreate;
+		for (let option of optionsToCreate) {
+			let optionParams = {
+				input: option,
+			}
+			console.log("inserting option: ", optionParams)
+			const createOptionResult = await API.graphql(
+				graphqlOperation(createOptionMutation, optionParams)
+			)
+			optionsList.push(createOptionResult.data.createOption)
+			console.log(createOptionResult)
+		}
 
-        let optionsList = [];
+		const pollText = generatePollText(poll, optionsList)
 
-        for (let option of optionsToCreate) {
-            let optionParams = {
-                input: option
-            }
-            console.log("inserting option: ", optionParams);
-            const createOptionResult = await API.graphql(
-                graphqlOperation(createOptionMutation, optionParams)
-            );
-            optionsList.push(createOptionResult.data.createOption);
-            console.log(createOptionResult);
-        }
+		setGeneratedPoll(pollText)
+		setIsPollGenerated(true)
+	}
 
+	const copyGeneratedPollIntoClipboard = () => {
+		copy(generatedPoll)
+	}
 
-        const pollText = generatePollText(poll, optionsList);
+	return (
+		<Grid overflow="auto" dir="vertical" gap={"small"}>
+			<Box basis="small" background={"dark"} dir="horizontal">
+				<Box align="center" pad="large" gap="medium">
+					<Text size="large" weight={"bold"}>
+						Create a Poll
+					</Text>
+					<Text size="small" textAlign="center" color={"secondary"}>
+						Fill in the fields below to create a poll.
+					</Text>
+					<Button href="/polls" label={"View My Polls"} />
+				</Box>
+				<Box background="bright" width="120%" height="5px"></Box>
+			</Box>
+			<Grid gap="small" pad={"small"} fill>
+				<TextInput
+					style={{
+						fontWeight: "normal",
+					}}
+					className="descriptionTitle"
+					placeholder={description === "" ? "What is your favorite color?" : ""}
+					value={description}
+					onChange={(e) => handleDescriptionChange(e.target.value)}
+					autoFocus={true}
+				/>
+				{options.map((option, index) => (
+					<Box
+						key={option.id}
+						direction="row"
+						align="center"
+						gap="medium"
+						margin={"small"}
+					>
+						<TextInput
+							style={{
+								fontWeight: "lighter",
+							}}
+							className="optionInput"
+							placeholder={
+								option.text === "" ? "Option " + (index + 1) : ""
+							}
+							value={option.text}
+							onChange={(e) =>
+								handleOptionChange(option.id, e.target.value)
+							}
+						/>
+						<Box alignContent="center" align="center">
+							<Button
+								pad={"none"}
+								style={{
+									borderRadius: "50%",
+								}}
+								onClick={() => removeOption(option.id)}
+								icon={<FormClose size="medium" color="secondary" />}
+							/>
+						</Box>
+					</Box>
+				))}
+				<Button
+					onClick={addOption}
+					label={"Add another option"}
+					icon={<FormAdd size="medium" />}
+				/>
+				<Button
+					style={{ fontWeight: "bolder" }}
+					primary
+					color={"bright"}
+					onClick={createPoll}
+					label={"Generate poll"}
+				/>
 
-        setGeneratedPoll(pollText);
-        setIsPollGenerated(true);
-    }
-
-    const copyGeneratedPollIntoClipboard = () => {
-        copy(generatedPoll);
-    }
-
-    return (
-        <Grid overflow='auto' dir='vertical' gap={'small'}>
-            <Box basis='small' background={'dark'}>
-                <Box align='center' pad='large' gap='medium'>
-                    <Text size='large' weight={'bold'} >Create a Poll</Text>
-                    <Text size='small' textAlign='center' color={'secondary'}>Fill in the fields below to create a poll.</Text>
-                </Box>
-                <Box background='bright' width='120%' height='5px'></Box>
-            </Box>
-            <Grid gap="small" pad={'small'} fill>
-                <TextInput
-                    style={{
-                        fontWeight: 'normal'
-                    }}
-                    className='descriptionTitle'
-                    placeholder={description === "" ? "What is your favorite color?" : ""}
-                    value={description}
-                    onChange={e => handleDescriptionChange(e.target.value)}
-                    autoFocus={true}
-                />
-                {options.map((option, index) => (
-                    <Box key={option.id} direction='row' align='center' gap='medium' margin={'small'}>
-                        <TextInput
-                            style={{
-                                fontWeight: 'lighter'
-                            }}
-                            className='optionInput'
-                            placeholder={option.text === "" ? "Option " + (index + 1) : ""}
-                            value={option.text}
-                            onChange={e => handleOptionChange(option.id, e.target.value)}
-                        />
-                        <Box alignContent='center' align='center'>
-                            <Button
-                                pad={'none'}
-                                style={{
-                                    borderRadius: '50%'
-                                }}
-                                onClick={() => removeOption(option.id)}
-                                icon={<FormClose size='medium' color='secondary' />}
-                            />
-                        </Box>
-                    </Box>
-                ))}
-                <Button onClick={addOption} label={"Add another option"} icon={<FormAdd size='medium' />} />
-                <Button style={{ fontWeight: 'bolder' }} primary color={'bright'} onClick={createPoll} label={'Generate poll'} />
-
-                {/* Display generated poll text. */}
-                {isPollGenerated ? (
-                    <Box gap="medium">
-                        <textarea
-                            readOnly
-                            style={{ resize: "none" }}
-                            rows={options.length * 3 + 1}
-                            value={generatedPoll}
-                        />
-                        <Button primary onClick={copyGeneratedPollIntoClipboard} label="Copy poll" />
-                    </Box>
-                ) : (
-                    ""
-                )}
-            </Grid>
-        </Grid>
-    );
+				{/* Display generated poll text. */}
+				{isPollGenerated ? (
+					<Box gap="medium">
+						<textarea
+							readOnly
+							style={{ resize: "none" }}
+							rows={options.length * 3 + 1}
+							value={generatedPoll}
+						/>
+						<Button
+							primary
+							onClick={copyGeneratedPollIntoClipboard}
+							label="Copy poll"
+						/>
+					</Box>
+				) : (
+					""
+				)}
+			</Grid>
+		</Grid>
+	)
 }
 
-export default PollOptions;
+export default PollOptions
--- src/components/ViewPolls.js
+++ src/components/ViewPolls.js
@@ -1,124 +1,136 @@
-import React, { useEffect, useState } from 'react';
-import { API, graphqlOperation } from 'aws-amplify';
-import { Box, Button, DropButton, Grid, Text, Tip } from 'grommet';
-import { generatePollText, getOrSetUserId } from '../utility/utilities';
-import { Clone, Info, Trash } from 'grommet-icons';
-import PollBar from './PollBar';
-import { pollsByDateWithOptions } from '../graphql/custom-queries';
-import copy from 'copy-to-clipboard';
-import { deletePoll as deletePollMutation } from '../graphql/mutations';
-
+import React, { useEffect, useState } from "react"
+import { API, graphqlOperation } from "aws-amplify"
+import { Box, Button, DropButton, Grid, Text, Tip } from "grommet"
+import { generatePollText, getOrSetUserId } from "../utility/utilities"
+import { Clone, Info, Trash } from "grommet-icons"
+import PollBar from "./PollBar"
+import { pollsByDateWithOptions } from "../graphql/custom-queries"
+import copy from "copy-to-clipboard"
+import { deletePoll as deletePollMutation } from "../graphql/mutations"
 
 const ViewPolls = ({ creatorId }) => {
-    const [polls, setPolls] = useState([]);
-    const [error, setError] = useState(null);
+	const [polls, setPolls] = useState([])
+	const [error, setError] = useState(null)
 
-    useEffect(() => {
-        async function fetchPolls() {
-            try {
-                const userId = getOrSetUserId();
+	useEffect(() => {
+		async function fetchPolls() {
+			try {
+				const userId = getOrSetUserId()
 
-                const response = await API.graphql(
-                    graphqlOperation(pollsByDateWithOptions, { creatorId: userId, sortDirection: "DESC" })
-                );
+				const response = await API.graphql(
+					graphqlOperation(pollsByDateWithOptions, {
+						creatorId: userId,
+						sortDirection: "DESC",
+					})
+				)
 
-                if (response.data.pollsByDate && response.data.pollsByDate.items) {
-                    console.log("Retrieved Polls: ", response.data.pollsByDate.items);
-                    setPolls(response.data.pollsByDate.items);
-                } else {
-                    // Handle the case where no polls are returned
-                    setPolls([]);
-                }
-            } catch (error) {
-                console.error('Error fetching polls:', error);
-                setError(error); // Store the error in state for rendering
-            }
-        }
+				if (response.data.pollsByDate && response.data.pollsByDate.items) {
+					console.log("Retrieved Polls: ", response.data.pollsByDate.items)
+					setPolls(response.data.pollsByDate.items)
+				} else {
+					// Handle the case where no polls are returned
+					setPolls([])
+				}
+			} catch (error) {
+				console.error("Error fetching polls:", error)
+				setError(error) // Store the error in state for rendering
+			}
+		}
 
-        fetchPolls();
-    }, [creatorId]);
+		fetchPolls()
+	}, [creatorId])
 
-    const getTotalVotesOnPoll = (poll) => {
-        let totalVotes = 0;
-        for (let option of poll.options.items) {
-            totalVotes += option.numVotes;
-        }
+	const getTotalVotesOnPoll = (poll) => {
+		let totalVotes = 0
+		for (let option of poll.options.items) {
+			totalVotes += option.numVotes
+		}
 
-        return totalVotes;
-    }
+		return totalVotes
+	}
 
-    const getVotePercent = (numVotes, totalVotes) => {
-        return totalVotes > 0 ? ((numVotes / totalVotes) * 100).toFixed(1) : 0;
-    }
+	const getVotePercent = (numVotes, totalVotes) => {
+		return totalVotes > 0 ? ((numVotes / totalVotes) * 100).toFixed(1) : 0
+	}
 
-    const displayNoPolls = () => {
-        return (
-            <Box>You have no polls.</Box>
-        )
-    }
+	const displayNoPolls = () => {
+		return <Box>You have no polls.</Box>
+	}
 
-    const deletePoll = async (pollId) => {
-        console.log("deletePoll: ", pollId);
-        const deletePollParams = {
-            input: { id: pollId }
-        }
+	const deletePoll = async (pollId) => {
+		console.log("deletePoll: ", pollId)
+		const deletePollParams = {
+			input: { id: pollId },
+		}
 
-        const deletePollResult = await API.graphql(
-            graphqlOperation(deletePollMutation, deletePollParams)
-        );
+		const deletePollResult = await API.graphql(
+			graphqlOperation(deletePollMutation, deletePollParams)
+		)
 
-        console.log("deleted poll result: ", deletePollResult);
+		console.log("deleted poll result: ", deletePollResult)
+	}
 
-    }
+	const listPolls = () => {
+		let listedPolls = []
+		for (let poll of polls) {
+			const totalVotes = getTotalVotesOnPoll(poll)
+			listedPolls.push(listPoll(poll, totalVotes))
+		}
 
-    const listPolls = () => {
-        let listedPolls = [];
-        for (let poll of polls) {
-            const totalVotes = getTotalVotesOnPoll(poll);
-            listedPolls.push(listPoll(poll, totalVotes))
-        }
+		return listedPolls
+	}
 
-        return listedPolls;
-    }
+	const listPoll = (poll, totalVotes) => {
+		const options = poll.options.items
+		return (
+			<Grid margin="medium" gap="small" key={"poll-" + poll.id}>
+				<Box direction="row">
+					<h3>{poll.description}</h3>
+					<Button
+						size="small"
+						icon={<Clone />}
+						onClick={() => {
+							copy(generatePollText(poll, options))
+						}}
+					/>
+					<Button
+						size="small"
+						icon={<Trash />}
+						onClick={() => {
+							deletePoll(poll.id)
+						}}
+					/>
+				</Box>
+				{options.map((option) => (
+					<PollBar option={option} totalVotes={totalVotes} />
+				))}
+				<Text size="small" color={"faint"}>
+					{totalVotes} votes
+				</Text>
+			</Grid>
+		)
+	}
 
-    const listPoll = (poll, totalVotes) => {
-        const options = poll.options.items;
-        return (
-            <Grid margin="medium" gap="small" key={'poll-' + poll.id}>
-                <Box direction='row'>
-                    <h3>{poll.description}</h3>
-                    <Button size='small' icon={<Clone />}
-                        onClick={() => { copy(generatePollText(poll, options)) }}
-                    />
-                    <Button size='small' icon={<Trash />}
-                        onClick={() => { deletePoll(poll.id) }}
-                    />
-                </Box>
-                {
-                    options.map((option) => (
-                        (
-                            <PollBar option={option} totalVotes={totalVotes} />
-                        )
-                    ))
-                }
-                <Text size='small' color={'faint'}>{totalVotes} votes</Text>
-            </Grid >
-        )
-    }
+	if (error) {
+		return <div>Error fetching polls: {error.message}</div>
+	}
 
-    if (error) {
-        return <div>Error fetching polls: {error.message}</div>;
-    }
-
-    return (
-        <Box overflow="auto">
-            <Grid>
-                {
-                    polls.length !== 0 ? listPolls() : displayNoPolls()
-                }
-            </Grid>
-        </Box>
-    );
+	return (
+		<Box>
+			<Button
+				margin={{ horizontal: "large", vertical: "medium" }}
+				href={"/"}
+				//justify={"evenly"}
+				primary
+				color={"bright"}
+				label={"Create New Poll +"}
+				alignSelf={"center"}
+			/>
+			<Box overflow="auto">
+				<Grid>{polls.length !== 0 ? listPolls() : displayNoPolls()}</Grid>
+			</Box>
+		</Box>
+	)
 }
 
-export default ViewPolls;
+export default ViewPolls
